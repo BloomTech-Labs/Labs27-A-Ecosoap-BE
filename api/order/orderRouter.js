a8946a571eefe3ebeeb8f201aab8a9de75ad6ed0
William Sedlacek <wsedlacekc@gmail.com>
2020-10-08 14:25:44 -0700

    refactor: clean up types
    This uses JS Doc TypeScript annotations to clean up typing and provides auto complete
    
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- api/order/orderRouter.js
+++ api/order/orderRouter.js
@@ -1,49 +1,50 @@
+// @ts-check
+
 const express = require('express');
+const { v4 } = require('uuid');
+
 const authRequired = require('../middleware/authRequired');
 const Orders = require('./orderModel');
 const router = express.Router();
 
-router.get('/', authRequired, function (req, res) {
-  Orders.findAll()
-    .then((orders) => {
-      res.status(200).json(orders);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({ message: err.message });
-    });
+router.get('/', authRequired, async (req, res) => {
+  try {
+    const orders = await Orders.findAll();
+    res.status(200).json(orders);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: err.message });
+  }
 });
 
-router.get('/:id', authRequired, function (req, res) {
-  const id = String(req.params.id);
-  Orders.findById(id)
-    .then((order) => {
-      if (order) {
-        res.status(200).json(order);
-      } else {
-        res.status(404).json({ error: 'OrderNotFound' });
-      }
-    })
-    .catch((err) => {
-      res.status(500).json({ error: err.message });
-    });
+router.get('/:id', authRequired, async (req, res) => {
+  const { id } = req.params;
+  try {
+    const order = await Orders.findById(id);
+
+    if (order) {
+      res.status(200).json(order);
+    } else {
+      res.status(404).json({ error: 'OrderNotFound' });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 router.post('/', authRequired, async (req, res) => {
   const newOrder = req.body;
   if (newOrder) {
-    const id = newOrder.id || 0; //0 needs to be replaced with a string
+    const id = newOrder.id || v4();
     try {
-      await Orders.findById(id).then(async (ordr) => {
-        if (ordr === undefined) {
-          //profile not found so lets insert it
-          await Orders.create(newOrder).then((order) =>
-            res.status(200).json({ message: 'order created', order: order[0] })
-          );
-        } else {
-          res.status(400).json({ message: 'order already exists' });
-        }
-      });
+      const existingOrder = await Orders.findById(id);
+
+      if (!existingOrder) {
+        const [order] = await Orders.create({ ...newOrder, id });
+        res.status(200).json({ message: 'order created', order: order });
+      } else {
+        res.status(400).json({ message: 'order already exists' });
+      }
     } catch (e) {
       console.error(e);
       res.status(500).json({ message: e.message });
@@ -53,44 +54,40 @@ router.post('/', authRequired, async (req, res) => {
   }
 });
 
-router.put('/', authRequired, function (req, res) {
+router.put('/', authRequired, async (req, res) => {
   const order = req.body;
   if (order) {
-    const id = order.id || 0; // 0 needs to be replaced
-    Orders.findById(id)
-      .then(
-        Orders.update(id, order)
-          .then((updated) => {
-            res
-              .status(200)
-              .json({ message: 'order created', order: updated[0] });
-          })
-          .catch((err) => {
-            res.status(500).json({
-              message: `Could not update order '${id}'`,
-              error: err.message,
-            });
-          })
-      )
-      .catch((err) => {
-        res.status(404).json({
-          message: `Could not find profile '${id}'`,
-          error: err.message,
-        });
+    const id = order.id || v4();
+
+    try {
+      await Orders.findById(id);
+    } catch (err) {
+      res.status(404).json({
+        message: `Could not find profile '${id}'`,
+        error: err.message,
       });
+    }
+
+    try {
+      const [updated] = await Orders.update(id, order);
+      res.status(200).json({ message: 'order created', order: updated });
+    } catch (err) {
+      res.status(500).json({
+        message: `Could not update order '${id}'`,
+        error: err.message,
+      });
+    }
   }
 });
 
-router.delete('/:id', authRequired, function (req, res) {
-  const id = req.params.id;
+router.delete('/:id', authRequired, async (req, res) => {
+  const { id } = req.params;
   try {
-    Orders.findById(id).then((order) => {
-      Orders.remove(order.id).then(() => {
-        res
-          .status(200)
-          .json({ message: `Order '${id}' was deleted.`, order: order });
-      });
-    });
+    const order = await Orders.findById(id);
+    await Orders.remove(id);
+    res
+      .status(200)
+      .json({ message: `Order '${id}' was deleted.`, order: order });
   } catch (err) {
     res.status(500).json({
       message: `Could not delete order with ID: ${id}`,
