59dbeee47f0f027a829852dd25bcd69f12c6cc5a
William Sedlacek <wsedlacekc@gmail.com>
2020-10-21 09:32:53 -0700

    test: support buyers
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- __tests__/routes/profiles.test.js
+++ __tests__/routes/profiles.test.js
@@ -20,18 +20,18 @@ describe('profiles router endpoints', () => {
 
   describe('GET /profiles', () => {
     it('should return 200', async () => {
-      Profiles.findAll.mockResolvedValue([]);
+      Profiles.findAllBuyers.mockResolvedValue([]);
       const res = await request(server).get('/profiles');
 
       expect(res.status).toBe(200);
       expect(res.body.length).toBe(0);
-      expect(Profiles.findAll.mock.calls.length).toBe(1);
+      expect(Profiles.findAllBuyers.mock.calls.length).toBe(1);
     });
   });
 
   describe('GET /profiles/:id', () => {
     it('should return 200 when profile found', async () => {
-      Profiles.findById.mockResolvedValue({
+      Profiles.findBuyerById.mockResolvedValue({
         id: 'd376de0577681ca93614',
         name: 'Bob Smith',
         email: 'bob@example.com',
@@ -40,11 +40,11 @@ describe('profiles router endpoints', () => {
 
       expect(res.status).toBe(200);
       expect(res.body.name).toBe('Bob Smith');
-      expect(Profiles.findById.mock.calls.length).toBe(1);
+      expect(Profiles.findBuyerById.mock.calls.length).toBe(1);
     });
 
     it('should return 404 when no user found', async () => {
-      Profiles.findById.mockResolvedValue();
+      Profiles.findBuyerById.mockResolvedValue();
       const res = await request(server).get('/profiles/d376de0577681ca93614');
 
       expect(res.status).toBe(404);
@@ -60,15 +60,15 @@ describe('profiles router endpoints', () => {
         avatarUrl:
           'https://s3.amazonaws.com/uifaces/faces/twitter/hermanobrother/128.jpg',
       };
-      Profiles.findById.mockResolvedValue(undefined);
-      Profiles.create.mockResolvedValue(
+      Profiles.findBuyerById.mockResolvedValue(undefined);
+      Profiles.createBuyer.mockResolvedValue(
         Object.assign({ id: 'd376de0577681ca93614' }, profile)
       );
       const res = await request(server).post('/profile').send(profile);
 
       expect(res.status).toBe(200);
       expect(res.body.profile.id).toBe('d376de0577681ca93614');
-      expect(Profiles.create.mock.calls.length).toBe(1);
+      expect(Profiles.createBuyer.mock.calls.length).toBe(1);
     });
   });
 
@@ -81,13 +81,13 @@ describe('profiles router endpoints', () => {
         avatarUrl:
           'https://s3.amazonaws.com/uifaces/faces/twitter/hermanobrother/128.jpg',
       };
-      Profiles.findById.mockResolvedValue(profile);
-      Profiles.update.mockResolvedValue(profile);
+      Profiles.findBuyerById.mockResolvedValue(profile);
+      Profiles.updateBuyer.mockResolvedValue(profile);
 
       const res = await request(server).put('/profile/').send(profile);
       expect(res.status).toBe(200);
       expect(res.body.profile.name).toBe('Louie Smith');
-      expect(Profiles.update.mock.calls.length).toBe(1);
+      expect(Profiles.updateBuyer.mock.calls.length).toBe(1);
     });
   });
 });
